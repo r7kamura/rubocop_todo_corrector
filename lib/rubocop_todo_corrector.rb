88b2383c2f57c4138ebdbe28655a403263182a6e
Ryo Nakamura <r7kamura@gmail.com>
2022-05-14 14:26:39 +0900

    Run: bundle gem --test=rspec rubocop_todo_corrector

---
+++ lib/rubocop_todo_corrector.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rubocop_todo_corrector/version"
+
+module RubocopTodoCorrector
+  class Error < StandardError; end
+  # Your code goes here...
+end
