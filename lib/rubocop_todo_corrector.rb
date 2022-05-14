695c93a365bbc4675dd77df18b41516cc6637435
Ryo Nakamura <r7kamura@gmail.com>
2022-05-14 15:16:58 +0900

    Run: rubocop -a

--- lib/rubocop_todo_corrector.rb
+++ lib/rubocop_todo_corrector.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "rubocop_todo_corrector/version"
+require_relative 'rubocop_todo_corrector/version'
 
 module RubocopTodoCorrector
   class Error < StandardError; end
